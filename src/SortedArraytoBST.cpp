3e2ff668a8d0ca35fe05a9231ae003a633b4b3d9
Rohith24 <Rohith24@missionrnd.com>
2016-03-26 16:56:42 +0530

    Submit Commit

--- src/SortedArraytoBST.cpp
+++ src/SortedArraytoBST.cpp
@@ -32,10 +32,29 @@ struct node{
 	int data;
 	struct node *right;
 };
-
+struct node* createnewNode(int data)
+{
+	struct node* node = (struct node*)malloc(sizeof(struct node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+	return node;
+}
+struct node* sortedArrayToBST(int *arr, int start, int end)
+{
+	if (start > end)
+		return NULL;
+	int mid = (start + end) / 2;
+	struct node *root = createnewNode(arr[mid]);
+	root->left = sortedArrayToBST(arr, start, mid - 1);
+	root->right = sortedArrayToBST(arr, mid + 1, end);
+	return root;
+}
 
 struct node * convert_array_to_bst(int *arr, int len){
-	
+	int start = 0, end = len;
+	if (arr==NULL)
 	return NULL;
+	return sortedArrayToBST(arr, start, end);
 }
 
